92543cd708ecc884d92cee11fec298c5b648d4bd
gabriel costa <gabriel.costa513@gmail.com>
2023-06-24 23:32:43 -0300

    :sparkles: adicionando database_credentials

---
+++ src/database_credentials.py
@@ -0,0 +1,9 @@
+db_user = 'your_username'
+db_password = 'your_password'
+
+db_host = 'localhost'
+db_port = '3306'
+db_name = 'your_database_name'
+
+# Create a connection string
+db_url = f'mysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}'
