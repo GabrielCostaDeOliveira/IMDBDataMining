4982198cf599992d75639d1ef0d00f7b6eb18a85
gabriel costa <gabriel.costa513@gmail.com>
2023-07-19 23:24:21 -0300

    :fire: deletando fotos antigas

--- src/database_credentials.py
+++
@@ -1,9 +0,0 @@
-db_user = 'root'
-db_password = 'cosmo'
-
-db_host = 'localhost'
-db_port = '3306'
-db_name = 'imdb_ijs'
-
-# Create a connection string
-db_url = f'mysql+mysqlconnector://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}'
